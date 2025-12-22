3ca85767a321e527e3b75ca0d479b4b2365ab275
) <qqianyu1037@gmail.com>
2025-12-22 20:23:36 +0800

    update arrow

--- web/page/Admin/admin_order.php
+++ web/page/Admin/admin_order.php
@@ -161,19 +161,27 @@ $_title = "Admin Orders | Four Eyes Collective";
         function sortLink($column, $label, $currentSort, $currentDir, $search, $status_filter)
         {
             $dir = 'asc';
-            $arrow = '';
+            $class = '';
+
             if ($currentSort === $column) {
                 if ($currentDir === 'asc') {
+                    $class = 'asc';   
                     $dir = 'desc';
-                    $arrow = ' ▲';
                 } else {
+                    $class = 'desc';  
                     $dir = 'asc';
-                    $arrow = ' ▼';
                 }
             }
-            $url = "?page=1&search=" . urlencode($search) . "&status=" . $status_filter . "&sort={$column}&dir={$dir}";
-            return "<a href='{$url}' style='text-decoration:none; color:inherit;'>{$label}{$arrow}</a>";
+
+            $url = "?page=1"
+                . "&search=" . urlencode($search)
+                . "&status=" . urlencode($status_filter)
+                . "&sort={$column}"
+                . "&dir={$dir}";
+
+            return "<a href='{$url}' class='{$class}'>{$label}</a>";
         }
+
         ?>
 
         <?php if (!empty($success_msg)): ?>
