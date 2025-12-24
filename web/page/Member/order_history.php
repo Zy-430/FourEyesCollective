1408e72c627eee245b4e0ee3decaed47f0d0be49
356Lqyu <raclim356@gmail.com>
2025-12-25 01:46:57 +0800

    update quak

--- web/page/Member/order_history.php
+++ web/page/Member/order_history.php
@@ -95,8 +95,6 @@ include '../../_head.php';
 
 <section class="hero-section">
     <div class="hero-inner">
-            <i class="fas fa-arrow-left"></i> Back
-        </a>
 
         <h1 class="hero-title">My Orders</h1>
         <p class="hero-subtitle">Track your orders, view history, and see details of each purchase.</p>
