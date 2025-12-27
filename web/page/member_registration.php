6805e6e5b85e7473a9bbc891c010fb5940493d4f
356Lqyu <raclim356@gmail.com>
2025-12-27 09:04:23 +0800

    Update member_registration.php

--- web/page/member_registration.php
+++ web/page/member_registration.php
@@ -44,7 +44,7 @@ if (is_post()) {
     $password = trim($password);
 
     if (!is_strong_password($password)) {
-        $_err['password'] == 'Invalid password format';
+        $_err['password'] = 'Invalid password format';
     }
 
     // Validate Confirm password 
